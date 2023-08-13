f06b4ee8fc5927facf3da1686f126405d6f59624
redachahdi <reda.elkhattabi01@gmail.com>
2023-08-13 07:51:26 -0700

    task 9

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,44 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""that Defines the FileStorage of the
+class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+from models.place import Place
+
 
 class FileStorage:
-    """Represents an abstracted storage engine.
-
-    Attributes:
-        _file_path (str): The name of the file to save objects into.
-        _objects (dict): A dictionary of instantiated objects.
+    """Defines an abstract storage engine.
+Attributes:
+- _file_path (str): The filename for storing objects.
+- _objects (dict): A dictionary containing instantiated objects.
     """
     _file_path = "file.json"
     _objects = {}
 
     def all(self):
-        """Returns the dictionary of objects."""
+        """that Returns the dictionary of the objects."""
         return FileStorage._objects
 
     def new(self, obj):
-        """Adds obj to _objects with key <obj_class_name>.id"""
+        """Adds obj to _objects with 
+        keys"""
         class_name = obj.__class__.__name__
         key = "{}.{}".format(class_name, obj.id)
         FileStorage._objects[key] = obj
 
     def save(self):
-        """Serializes _objects to the JSON file _file_path."""
+        """that Serializes the _objects to the JSON fil"""
         object_dict = {key: obj.to_dict() for key, obj in FileStorage._objects.items()}
         with open(FileStorage._file_path, "w") as file:
             json.dump(object_dict, file)
 
     def reload(self):
-        """Deserializes the JSON file _file_path to _objects, if it exists."""
+        """if the Deserializes the JSON file _file_path to _objects, """
         try:
             with open(FileStorage._file_path) as file:
                 object_dict = json.load(file)
