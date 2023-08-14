a5f1c67a37db1678efc428cd9434cfa9c61b45a1
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-08-14 01:11:38 +0100

    airbnb

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Represents a review.
     Attributes:
-        place_id (str): The Place id.
+        place_id  (str): The Place id.
         user_id  (str): The User id.
         text (str): The text of the review.
     """
